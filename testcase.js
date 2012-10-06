9eba0a4e21f3be63562fa02640647cdcc65ab91a
sofish <happinesz@gmail.com>
2012-10-07 04:48:54 +0800

    add pinyin support

--- testcase.js
+++ testcase.js
@@ -1,15 +1,22 @@
-var han = require('./han');
+var han = require('./han')
 
-console.log('原文：中文\n' + han.letter('中文'), '\n')
-console.log('原文：要实现 Speaker Deck 那种中文转拼音的\n' + han.letter('要实现 Speaker Deck 那种中文转拼音的', '-'), '\n')
+console.log('original：中文\n' + han.letter('中文'), '\n')
+console.log('original：要实现 Speaker Deck 那种中文转拼音的\n' + han.letter('要实现 Speaker Deck 那种中文转拼音的', '-'), '\n')
 
 han.letter('中aaaaa中¢∞§¶•誩aa文喳aa', function(err, result){
 	if(err) throw err;
-	console.log('原文：中aaaaa中¢∞§¶•誩aa文喳aa')
-	console.log('异步结果：' + result, '\n')
-});
+	console.log('original：中aaaaa中¢∞§¶•誩aa文喳aa')
+	console.log('callback：' + result, '\n')
+})
 
 han.letter('中EnglishWords¢∞§¶•ªº文', '-', function(err, result){
-	console.log('原文：中EnglishWords¢∞§¶•ªº文')
-	console.log('异步结果：' + result)
-});
+	console.log('original：中EnglishWords¢∞§¶•ªº文')
+	console.log('callback：' + result, '\n')
+})
+
+console.log('original: My Chinese name is 小鱼(sofish)')
+console.log(han.pinyin('My Chinese name is 小鱼(sofish)'))
+console.log('');
+
+console.log('original: #$%^&*中23¢∞§¶•ª52849文@#$%^&*(意思，还有英文：english')
+console.log(han.pinyin('#$%^&*中23¢∞§¶•ª52849文@#$%^&*(意思，还有英文：english'))
